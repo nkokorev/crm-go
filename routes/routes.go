6068e486217a1745329a718f211b7ac5d78ad22b
nkokorev <kokorevn@gmail.com>
2020-04-19 12:53:02 +0300

    prefix routing and middleware

--- routes/routes.go
+++ routes/routes.go
@@ -38,9 +38,9 @@ func Handlers() *mux.Router {
 	rApp := rBase.Host("app." + crmHost).PathPrefix("/ui-api").Subrouter()                                  // APP [app.ratuscrm.com/ui-api]
 	rUiApi := rBase.Host("ui.api." + crmHost).PathPrefix("/accounts/{accountHashId:[a-z0-9]+}").Subrouter() // UI/API [ui.api.ratuscrm.com]
 
-	/**
+	/******************************************************************************************************************
 
-		###	Authentication user ###
+		###	Authentication user rules ###
 
 		1. В App базовая авторизация производится в RatusCRM аккаунте.
 		2. На этапе выдачи account-token issuerAccount становится равным аккаунту, в котором авторизован пользователь.
@@ -51,118 +51,41 @@ func Handlers() *mux.Router {
 		1. issuerAccount - auth Account | (*models.Account), example: issuerAccount.DecryptToken(token string) (error, token)
 		2. account - target (load/work) Account | (*models.Account), example: account.LoginUser(username, password string)
 		3. user - auth user in work account | (*models.User). Example: user.CreateAccount(Account{...})
+		4. issuer - channel of request: 'app', 'api', 'ui-api'. Need for some logic controllers (^_^)
 
 		* All request of App decrypting with RatusCRM AES/JWT key. Add to context(r) issuerAccount
 		* All request of UI decrypting with Account AES/JWT key. Account add to context(r) issuerAccount from .../{account_hash_id}/...
-		* All request of Api check Bearer in Headers(r) and compare in api-key table
+		* All request of Api check Bearer token in Headers(r) and compare in api-key table
 
-		##	F
+		### Middleware(r) ###
 
-	*/
+		1. middleware.CheckApiStatus - проверяет статус API для всех аккаунтов
+		2. middleware.CheckAppUiApiStatus - проверяет статус App UI/API в настройках GUI RatusCRM
+		3. middleware.CheckUiApiStatus - проверяет статус Public UI/API для всех аккаунтов
 
-	/*
+		- signedAccount
 
-		JWT/AES key:
-					issuerAccountId - id аккаунта, который выдает авторизацию. В App UI RatusCRM всегда = 1.
-					accountId 		- id аккаунта, в котором пользователь работает (в UI/API совпадает с issuerAccountId).
-					userId 			- id авторизованного пользователя.
+		4. middleware.ContextMuxVarAccount - Вставляет в контекст issuerAccountId из hashAccountId (раскрытие issuer account) && issuerAccount
+		5. middleware.ContextMainAccount - устанавливает в контекст issuerAccountId = 1 && issuerAccount
 
-		  			issuerAccount	- аккаунт откуда пришел запрос
-					account			- аккаунт, в котором авторизован пользователь
-					user			- пользователь, прошедший авторизацию
+		- Authentication
 
-		authAccount - авторизующий аккаунт (используются его AES/JWT ключи).
-		Context: [issuer]: 'app' / 'api' / 'ui-api'.
+	  	6. middleware.BearerAuthentication - читает с проверкой JWT, проверяет статус API в аккаунте. Дополняет контекст accountId && account
+		7. middleware.JwtUserAuthentication - проверяет JWT и устанавливает в контекст userId & user
+		8. middleware.JwtFullAuthentication - проверяет JWT и устанавливает в контекст userId & user, accountId && account
 
-		# APP
-			url: app.ratuscrm.com/ui-api
-			auth: Bearer token
+	******************************************************************************************************************/
 
-		# UI/API
-			url: ui.api.ratuscrm.com.
-			auth: Bearer token
+	rApi.Use	(middleware.CorsAccessControl, 		middleware.CheckApiStatus, 		middleware.BearerAuthentication)
+	rApp.Use	(middleware.CheckAppUiApiStatus,	middleware.AddContextMainAccount)
+	rUiApi.Use	(middleware.CorsAccessControl, 		middleware.CheckUiApiStatus, 	middleware.ContextMuxVarAccountHashId)
 
-		# API
-			url: api.ratuscrm.com
-			auth: md5 hash
-
-
-	*/
-
-	/**
-		1. The request can have 4 statuses: guest, authUser, authAccount, fullAuth (user + account)
-		2. API has only authAccount
-		3. APP has 3 status: guest, authUser and fullAuth
-		4. UI/API has 2 status: guest, fullAuth.
-
-	 	Context statuses:
-			1. guest - has't userId or accountId
-			2. authUser: have userId & user (type of models.User)
-			3. authAccount: have accountId & account (type of models.Account)
-			4. fullAuth: have userId, accountId, user, account
-
-		Important notice: about use hash AES or JWT key:
-			1. API has't (clearly)
-			2. App use only RatusCRM AES/JWT key
-			3. UI/API use special AES/JWT key for him account (by accountId)
-	*/
-
-	// New route point for using middleware
-	// rAppAuthUser := rApp.PathPrefix("").Subrouter()
-	// rAppAuthFull := rApp.PathPrefix("").Subrouter()
-	// rUiApiAuthFull := rUiApi.PathPrefix("").Subrouter()
-
-	// #### Подключаем Middleware ####
-	/**
-
-		## Посредники проверяющие флаги настройки системы
-		middleware.CheckApiStatus - проверяет статус API для всех аккаунтов
-		middleware.CheckAppUiApiStatus - проверяет статус App UI/API в настройках GUI RatusCRM
-		middleware.CheckUiApiStatus - проверяет статус Public UI/API для всех аккаунтов
-
-		## Посредники определяющие signedAccount
-		middleware.ContextMuxVarAccount - Вставляет в контекст issuerAccountId из hashAccountId (раскрытие issuer account) && issuerAccount
-		middleware.ContextMainAccount - устанавливает в контекст issuerAccountId = 1 && issuerAccount
-
-		## Посредники работающие с авторизацией пользователя
-	  	middleware.BearerAuthentication - читает с проверкой JWT, проверяет статус API в аккаунте. Дополняет контекст accountId && account
-		middleware.JwtUserAuthentication - проверяет JWT и устанавливает в контекст userId & user
-		middleware.JwtFullAuthentication - проверяет JWT и устанавливает в контекст userId & user, accountId && account
-
-		--------------------------
-
-
-		Посредники добавляют в контексте следующую информацию:
-
-	  	issuer			- откуда пришел запрос: "app", "ui-api", "api"
-		issuerAccountId - id root аккаунта, от имени которого происходит запрос. В App RatusCRM всегда = 1.
-		accountId 		- id аккаунта, в котором пользователь авторизован. Равен 0, если не авторизован. В UI/API совпадает с issuerAccountId.
-		userId 			- id авторизованного пользователя. Равен 0, если не авторизован.
-
-	  	issuerAccount	- аккаунт откуда пришел запрос
-		account			- аккаунт, в котором авторизован пользователь
-		user			- пользователь, прошедший авторизацию
-	*/
-
-	// Все запросы API имеют в контексте accountId, account. У них нет userId.
-	rApi.Use(middleware.CorsAccessControl, middleware.CheckApiStatus, middleware.BearerAuthentication)
-
-	// All requests has AES/JWT key of main account.
-	rApp.Use(		 middleware.CheckAppUiApiStatus, middleware.AddContextMainAccount)
-	//rAppAuthUser.Use(middleware.CheckAppUiApiStatus, middleware.AddContextMainAccount, middleware.JwtCheckUserAuthentication)
-	//rAppAuthFull.Use(middleware.CheckAppUiApiStatus, middleware.AddContextMainAccount, middleware.JwtCheckFullAuthentication)
-
-	// Через UI/API запросы всегда идут в контексте аккаунта
-	rUiApi.Use(			middleware.CorsAccessControl, middleware.CheckUiApiStatus, middleware.ContextMuxVarAccountHashId)
-	//rUiApiAuthFull.Use( middleware.CorsAccessControl, middleware.CheckUiApiStatus, middleware.ContextMuxVarAccountHashId) // set userId,accountId,account
-
-	// ### Передаем запросы в обработку ###
+	// Route handlers
 	ApiRoutes(rApi)
-	//AppRoutes(rApp, rAppAuthUser, rAppAuthFull)
 	AppRoutes(rApp)
-	//UiApiRoutes(rUiApi, rUiApiAuthFull)
 	UiApiRoutes(rUiApi)
 
+	// ### 404 (^_^) ###
 	rBase.NotFoundHandler = middleware.NotFoundHandler()
 
 	return rBase
