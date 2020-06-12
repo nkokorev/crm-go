24561c7848baceceec18f05fef3d142ce5cbd508
nkokorev <kokorevn@gmail.com>
2020-06-12 22:39:56 +0300

    product attr 1

--- controllers/ShopCr.go
+++ controllers/ShopCr.go
@@ -583,45 +583,6 @@ func ProductUpdate(w http.ResponseWriter, r *http.Request) {
 	u.Respond(w, resp)
 }
 
-func ProductUpdateAttribute(w http.ResponseWriter, r *http.Request) {
-
-	account, err := GetWorkAccount(w,r)
-	if err != nil || account == nil {
-		u.Respond(w, u.MessageError(u.Error{Message:"Ошибка авторизации"}))
-		return
-	}
-
-	productId, err := GetUINTVarFromRequest(r, "productId")
-	if err != nil {
-		u.Respond(w, u.MessageError(err, "Ошибка в обработке ID товара"))
-		return
-	}
-
-	// var input interface{}
-	//var input map[string]interface{}
-	var input = struct {
-		Attributes map[string]interface{} `json:"attributes"`
-	}{}
-
-	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-		fmt.Println(err)
-		u.Respond(w, u.MessageError(err, "Техническая ошибка в запросе"))
-		return
-	}
-
-
-	//card, err := account.UpdateProduct(productId, input.Product)
-	card, err := account.UpdateProductAttribute(productId, input.Attributes)
-	if err != nil {
-		u.Respond(w, u.MessageError(err, "Ошибка при обновлении"))
-		return
-	}
-
-	resp := u.Message(true, "PATCH Product Update")
-	resp["product"] = card
-	u.Respond(w, resp)
-}
-
 func ProductDelete(w http.ResponseWriter, r *http.Request) {
 
 	account, err := GetWorkAccount(w,r)
