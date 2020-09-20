7282610a110c99da62ba0e4f53f315e2690545a7
Kokorev Nikita <kokorevn@gmail.com>
2020-09-20 18:17:46 +0300

    updating gorm..

--- models/product_card-product.go
+++ models/product_card-product.go
@@ -18,22 +18,25 @@ func (ProductCardProduct) PgSqlCreate() {
 	if err != nil {
 		log.Fatal("Error: ", err)
 	}
-
 }
 
 func (ProductCardProduct) BeforeCreate(db *gorm.DB) error {
-	// ...
 	return nil
 }
 
 func (productCard ProductCard) ExistProduct(product *Product) bool {
+
 	if product.Id < 1 {
 		return false
 	}
+
 	var count int64
+
 	db.Model(&ProductCardProduct{}).Where("product_id = ? AND product_card_id = ?", product.Id, productCard.Id).Count(&count)
+
 	if count > 0 {
 		return true
 	}
+	
 	return false
 }
