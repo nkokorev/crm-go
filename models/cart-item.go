57e7d9642413896ba89e6067b1d91f7b94db8810
Kokorev Nikita <kokorevn@gmail.com>
2020-10-30 01:59:32 +0300

    prepare

--- models/cart-item.go
+++ models/cart-item.go
@@ -508,25 +508,127 @@ func (cartItem CartItem) GetAvailabilityWarehouseItems() []WarehouseItem {
 	if cartItem.Id < 1 || cartItem.ProductId < 1 { return warehouseItems}
 
 	var product Product
-	if err := (Account{Id: cartItem.AccountId}).LoadEntity(&product, cartItem.ProductId, []string{"SourceItems"}); err != nil {
+	if err := (Account { Id: cartItem.AccountId }).LoadEntity(&product, cartItem.ProductId, []string{"SourceItems"}); err != nil {
 		return warehouseItems
 	}
 
-	fmt.Println("product S: ", product.SourceItems[0].Quantity)
+	/*fmt.Println("is Kit: ", product.IsKit)
+	fmt.Println("product S: ", product.SourceItems[0].Quantity)*/
 
 	// Это доставка, она доступна (по дефолту)
 	if cartItem.ProductId < 1 { return warehouseItems }
+	
+	if product.IsKit {
 
-	// if cartItem.Quantity == 0 { return warehouseItems }
+		/*log.Printf("\n-------------------")
+		log.Println("SourceItems: ", len(product.SourceItems))
+		log.Println("Quantity: ", product.SourceItems[0].Quantity)
+		log.Println("Source product Id: ", product.SourceItems[0].SourceId)
+		log.Println("Source product Id: ", product.SourceItems[1].SourceId)
+		log.Printf("\n")*/
 
-	err := db.Model(&WarehouseItem{}).
-		Where("product_id = ? AND stock >= ? AND stock > 0", cartItem.ProductId, cartItem.Quantity).
-		Preload("Warehouse").Find(&warehouseItems).Error
+		whItems := make([]WarehouseItem,0)
 
-	if err != nil && err != gorm.ErrRecordNotFound {
-		return warehouseItems
+		// list of warehouse Ids
+		warehouses := make([]uint,0)
+
+		for i := range product.SourceItems {
+			if product.SourceItems[i].ProductId > 0 {
+
+				// ProductId - id товара ДЛЯ которого собираем
+				// SourceId - id товаров ИЗ которых собираем
+
+				wIs := make([]WarehouseItem,0)
+
+				err := db.Model(&WarehouseItem{}).
+					Where("product_id = ? AND stock >= ? AND stock > 0", product.SourceItems[i].SourceId, product.SourceItems[i].Quantity).
+					Preload("Warehouse").Find(&wIs).Error
+
+				// Если какого-то товара нет, то сразу возвращаем  []
+				if err != nil || len(wIs) == 0 { return warehouseItems }
+
+				for y := range wIs {
+					whItems = append(whItems,wIs[y])
+
+					// Добавляем, если все товары есть
+					_, ok := FindUINT(warehouses, wIs[y].WarehouseId)
+					if !ok {
+						warehouses = append(warehouses, wIs[y].WarehouseId)
+					}
+				}
+
+			}
+		}
+
+		// Надо сравнить arr
+		for _, wItem := range whItems {
+
+			exist := false
+			for _,source := range product.SourceItems {
+				if source.SourceId == wItem.ProductId {
+					exist = true
+					continue
+				}
+			}
+			
+			// Не нашли склад для одного из source Item
+			if !exist { return warehouseItems }
+
+			// Запоминаем склад
+			fmt.Println("Запоминаем склад: ", wItem.WarehouseId)
+		}
+
+		// Перебираем по найденным id складов
+		/*for id, whId := range warehouses {
+
+			// если товар на этом складе
+			exist := false
+
+			// идем по найденным warehouseItems
+			for _,item := range whItems {
+				if whId == item.WarehouseId {
+					// fmt.Println("whId ", whId)
+					// fmt.Println("item.WarehouseId:", item.WarehouseId)
+					exist = true
+					continue
+				}
+			}
+
+			// товар не найден и склад надо исключить
+			if !exist {
+				fmt.Println("remove: ", id)
+				warehouses = RemoveIndex(warehouses,id)
+			}
+		}
+		*/
+		// fmt.Println("warehouses: ", warehouses)
+
+		for _, v := range whItems {
+			log.Printf("id: %v product_id: %v : warehouse_id: %v\n", v.Id, v.ProductId, v.WarehouseId)
+		}
+		
+	} else {
+		err := db.Model(&WarehouseItem{}).
+			Where("product_id = ? AND stock >= ? AND stock > 0", cartItem.ProductId, cartItem.Quantity).
+			Preload("Warehouse").Find(&warehouseItems).Error
+
+		if err != nil && err != gorm.ErrRecordNotFound {
+			return warehouseItems
+		}
 	}
-
+	
 	return warehouseItems
+}
 
+func FindUINT(slice []uint, val uint) (int, bool) {
+	for i, item := range slice {
+		if item == val {
+			return i, true
+		}
+	}
+	return -1, false
+}
+
+func RemoveIndex(s []uint, index int) []uint {
+	return append(s[:index], s[index+1:]...)
 }
